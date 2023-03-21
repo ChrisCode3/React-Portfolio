20c065bbbeb31c33b1dc0fd36443d8ff5eebde67
ChrisCode3 <malusdarkblade66@gmail.com>
2023-03-21 12:31:48 +0000

    added react app and bootstrap

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Hello from './components/about page/about';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+         hello darkness
+        </p>
+        <p>
+       hello
+        </p>
+      <Hello/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
